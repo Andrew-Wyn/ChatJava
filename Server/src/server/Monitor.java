9d26ca141c83bab0975c1a7dc326a32a8b774a64
andrew-wyn <terraformer.144@gmail.com>
2019-05-30 19:18:43 +0200

    server 2.0

--- Server/src/server/Monitor.java
+++ Server/src/server/Monitor.java
@@ -2,6 +2,7 @@ package server;
 
 // Monitor
 
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.*;
 import java.net.*;
@@ -26,5 +27,57 @@ public class Monitor {
     
     public void stop(){
         executor.shutdown();
+    }
+    
+    public synchronized void sendAllPartecipants(ThreadServer th){
+        String out = "";
+        for(ThreadServer thread : clients){
+            if(thread != th){
+                out += th.nome + "\n";
+            }
+        }
+        try{
+            th.dos.writeBoolean(false);
+            th.dos.writeUTF(out);
+            th.dos.flush(); 
+        } catch(IOException e){
+            
+        }
+        
+    }
+    
+    public synchronized void delete(ThreadServer th){
+        clients.remove(th);
+        System.out.println("oh no un client se ne e' andata fu**k");
+        try{
+            th.dos.writeBoolean(false);
+            th.dos.writeUTF(th.nome);
+            th.dos.flush();
+        }catch(IOException e){}
+    }
+    
+    public synchronized void broadCast(ThreadServer th, String msg){
+        for(ThreadServer thread : clients){
+            if(thread != th){
+                try{
+                    thread.dos.writeBoolean(true);
+                    thread.dos.writeUTF(th.nome + ">" + msg + "\n");
+                    thread.dos.flush();
+                }catch(IOException e){}
+            }
+        }
+    }
+    
+    public synchronized void ptp(String dest, String msg){
+        for(ThreadServer thread : clients){
+            if(thread.nome.equals(dest)){
+                try{
+                    thread.dos.writeBoolean(true);
+                    thread.dos.writeUTF(thread.nome + " [pvt]> " + msg + "\n");
+                    thread.dos.flush();
+                }catch(IOException e){}
+                return;
+            }
+        }
     }
 }
--- Server/src/server/ThreadServer.java
+++ Server/src/server/ThreadServer.java
@@ -8,20 +8,50 @@ import java.util.concurrent.*;
 public class ThreadServer implements Callable<Void>{
     public Socket client;
     private Monitor monitor;
+    DataInputStream dis;
+    DataOutputStream dos;
+    public String nome;
     
     public ThreadServer(Socket clients, Monitor monitor){
         this.client = clients;
         this.monitor = monitor;
+        
+        try{
+            dis = new DataInputStream(new BufferedInputStream(client.getInputStream()));
+            dos = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
+            
+            nome = dis.readUTF(); // leggo il nome dal client
+            monitor.sendAllPartecipants(this);
+            
+            
+        }catch(IOException e){}
+        
     }
     
     @Override
     public Void call() throws Exception {
-        DataInputStream dis = new DataInputStream(new BufferedInputStream(client.getInputStream()));
-        DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
+        try {
+            
+            while(true){
+                if(dis.readBoolean()){
+                    monitor.broadCast(this, dis.readUTF());
+                } else {
+                    String [] in = dis.readUTF().split("-");
+                    String dest = in[0].trim();
+                    monitor.ptp(dest, in[1].trim());
+                }
+                
+            }
+            
+        }catch(IOException e){
+            // scollegato
+        } finally{
+            monitor.delete(this);
+            dis.close();
+            dos.close();
+            client.close();
+        }
         
-        dos.writeUTF(dis.readUTF());
-        dos.flush();
-         
         return null;
     }
 }
